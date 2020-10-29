69f2db2bcf274dadf58a69072a5ebdf6fc954c01
mdemouch <mdemouch@gmail.com>
2020-10-29 12:00:07 +0000

    Mejorado el install y añadido HomeController

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ ruta=`pwd`
 composer create-project  --prefer-dist  laravel/laravel $app 8
 
 cd $app
+
+cp -R ../extra/* .
+
+chmod -R 777 storage/logs
+chmod -R 777 storage/framework
+
+
 #composer dump-autoload
 #composer install --no-scripts
 #composer update
@@ -13,6 +20,12 @@ cd $app
 php artisan -V
 
 npm install
+
+composer update
+
+php artisan migrate
+
+php artisan db:Seed
 
 #echo "Para añadir los enlaces simbolicos"
 #sudo ln -s $ruta/$app /var/www/html/$app
